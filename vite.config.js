6550cd50609019870c44622760f94bf7c1730fc7
Purushottam officec lapi <purushottam@menteso.com>
2026-03-12 00:01:48 +0530

    Rename JSX files to JS for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      async transform(code, id) {
+        if (!/\/src\/.*\.js$/.test(id)) {
+          return null
+        }
+
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        })
+      },
+    },
+    react(),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
